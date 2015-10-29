8894037650702a50ce40950fbbcfacc6e09fcd13
Praveen Kumar <praveen+git@kumar.in>
2015-10-29 01:49:02 -0700

    Implement query form in MO view.

--- pyaci/query.py
+++ pyaci/query.py
@@ -11,6 +11,7 @@ from collections import defaultdict
 from flask import Flask, request, render_template
 from flask.views import MethodView
 from .utils import getParentDn
+import os
 
 
 # TODO: Move to core.
@@ -64,32 +65,51 @@ class Index(object):
 
 
 class AView(MethodView):
-    def __init__(self, index):
+    def __init__(self, index, app):
         self._index = index
+        self._app = app
 
     @property
     def index(self):
         return self._index
+
+    @property
+    def app(self):
+        return self._app
+
+    @property
+    def appIndices(self):
+        return sorted(self.app._indices.keys())
 
 
 class DnView(AView):
     def get(self, dn):
         result = self.index.queryByDn(dn, **request.args)
         return render_template('mo-view.html', mos=result,
-                               indexName=self.index.name)
+                               indexName=self.index.name,
+                               indices=self.appIndices,
+                               currentDn=dn)
 
 
 class ClassView(AView):
     def get(self, className):
         result = self.index.queryByClass(className)
         return render_template('mo-view.html', mos=result,
-                               indexName=self.index.name)
+                               indexName=self.index.name,
+                               indices=self.appIndices,
+                               currentClassName=className)
 
 
 class App(object):
     def __init__(self):
         self._indices = {}
-        self._app = Flask('pyaci Query App')
+        templateFolder = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), 'templates')
+        staticFolder = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), 'static')
+        self._app = Flask('pyaci Query App',
+                          static_folder=staticFolder,
+                          template_folder=templateFolder)
         self._app.add_template_test(isDn, 'Dn')
         self._app.add_template_filter(getParentDn, 'parentDn')
 
@@ -98,15 +118,17 @@ class App(object):
         self._indices[index.name] = index
         self._app.add_url_rule(
             '/{}/dn/<path:dn>'.format(index.name),
-            view_func=DnView.as_view('dn_view', index),
+            view_func=DnView.as_view('dn_view_{}'.format(index.name),
+                                     index, self),
             methods=['GET']
         )
         self._app.add_url_rule(
             '/{}/class/<className>'.format(index.name),
-            view_func=ClassView.as_view('class_view', index),
+            view_func=ClassView.as_view('class_view_{}'.format(index.name),
+                                        index, self),
             methods=['GET']
         )
 
     def run(self, debug=False):
         self._app.debug = debug
-        self._app.run()
+        self._app.run(host='0.0.0.0')
