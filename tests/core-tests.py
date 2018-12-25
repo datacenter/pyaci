c2072cabff2c3e533a08af1a8b658d504629260e
Ravi R <toravir@yahoo.com>
2018-12-24 17:30:06 -0800

    auto refresh tests.

--- tests/core-tests.py
+++ tests/core-tests.py
@@ -339,7 +339,6 @@ class LoginTests(unittest.TestCase):
 <DnDomainMapEntry dn="uni/tn-infra" readPrivileges="admin" writePrivileges="admin"/>
 <DnDomainMapEntry dn="uni/tn-common" readPrivileges="admin" writePrivileges="admin"/>
 </aaaLogin></imdata>'''
-
         httpretty.register_uri(httpretty.POST,
                                'http://localhost/api/aaaLogin.xml',
                                body=xml_body,
@@ -350,13 +349,6 @@ class LoginTests(unittest.TestCase):
         (httpretty.last_request().method).should.equal('POST')
         (httpretty.last_request().path).should.equal('/api/aaaLogin.xml')
         (httpretty.last_request().body.decode('utf-8')).should.equal(self.login.Xml)
-
-        httpretty.register_uri(httpretty.POST,
-                               'http://localhost/api/aaaLogin.xml',
-                               body='',
-                               content_type='application/xml',
-                               status=404)
-        self.login.POST(format='xml')
 
 
 class LogoutTests(unittest.TestCase):
@@ -420,22 +412,40 @@ class LoginRefreshTests(unittest.TestCase):
 class AutoRefreshTests(unittest.TestCase):
     def setUp(self):
         self.node = pyaci.Node('http://localhost')
-        self.arth = pyaci.autoRefreshThread(self.node)
+        self.login = self.node.methods.Login('admin', 'password', autoRefresh=True)
 
     @httpretty.activate
     def testRefreshOnce(self):
-        xml_body = '''<?xml version="1.0" encoding="UTF-8"?><imdata totalCount="1">
+        login_xml_body = '''<?xml version="1.0" encoding="UTF-8"?><imdata totalCount="1">
+<aaaLogin token="f00AAAAAAAAAAAAAAAAAAK4wjyQODSwoW3hizW066Ts6Gs2S4fkFZf6XhK32II8gZrRrgVGF2Y0pPs05FntrA6LCwXFWicPGpgsUp+SqTJdHZMeQrn45HBxJrmSJKtuYiqCX5Qc5P67Qq+c4w+VDcsHZxXe7KqeUs1TfKlXvvco8CwPOCRWJzMly0ArRsEL6c4t5zQTYpy9XsGwQEWJD/A==" siteFingerprint="UAViQctMne4xvtyZ" refreshTimeoutSeconds="600" maximumLifetimeSeconds="86400" guiIdleTimeoutSeconds="1200" restTimeoutSeconds="90" creationTime="1545696032" firstLoginTime="1545696032" userName="admin" remoteUser="false" unixUserId="15374" sessionId="LlQAR9nARFiVBAJWpwrTBQ==" lastName="" firstName="" changePassword="no" version="4.1(0.90b)" buildTime="Fri Oct 26 16:18:38 PDT 2018" node="topology/pod-1/node-1">
+<aaaUserDomain name="all" rolesR="admin" rolesW="admin">
+<aaaReadRoles/>
+<aaaWriteRoles>
+<role name="admin"/>
+</aaaWriteRoles>
+</aaaUserDomain>
+<DnDomainMapEntry dn="uni/tn-mgmt" readPrivileges="admin" writePrivileges="admin"/>
+<DnDomainMapEntry dn="uni/tn-infra" readPrivileges="admin" writePrivileges="admin"/>
+<DnDomainMapEntry dn="uni/tn-common" readPrivileges="admin" writePrivileges="admin"/>
+</aaaLogin></imdata>'''
+        httpretty.register_uri(httpretty.POST,
+                               'http://localhost/api/aaaLogin.xml',
+                               body=login_xml_body,
+                               content_type='application/xml',
+                               status=200)
+
+        self.login.POST(format='xml')
+        refresh_xml_body = '''<?xml version="1.0" encoding="UTF-8"?><imdata totalCount="1">
 <aaaLogin token="700AAAAAAAAAAAAAAAAAAEdiM3Vap8h/9pkqbxgvrOKvPvYW8nkyXIAILMWqdcXSADxXZPE06nsifq+kslkI2UECxR+977d9+yaLtBhK0sz9ugT+id+GFVjh6irHCfIcQDAFeOEYo7u8hxqD84f6iIvGnDzQdZpD4256UkJAZHActeNQzVeDiVS5ldaSEqzAh1Df5IITKKASySUCHi71wg==" siteFingerprint="UAViQctMne4xvtyZ" refreshTimeoutSeconds="600" maximumLifetimeSeconds="86400" guiIdleTimeoutSeconds="1200" restTimeoutSeconds="90" creationTime="1545699898" firstLoginTime="1545699898" userName="admin" remoteUser="false" unixUserId="15374" sessionId="joR4ziDdTeedI1lybx1bjQ==" lastName="" firstName="" changePassword="no" version="4.1(0.90b)" buildTime="Fri Oct 26 16:18:38 PDT 2018" node="topology/pod-1/node-1">
 <aaaUserDomain name="all" readRoleBitmask="0" writeRoleBitmask="1"/>
 </aaaLogin></imdata>'''
         self.node._login['nextRefreshBefore']=int(time.time()) - 120
         httpretty.register_uri(httpretty.POST,
                                'http://localhost/api/aaaRefresh.xml',
-                               body=xml_body,
+                               body=refresh_xml_body,
                                content_type='application/xml',
                                status=200
                                )
-
         self.node._wsEvents={}
         self.node._wsEvents['123456789']=[]
         self.node._wsLastRefresh = int(time.time()) - 60
@@ -443,7 +453,7 @@ class AutoRefreshTests(unittest.TestCase):
                                'http://localhost/api/subscriptionRefresh.xml?id=123456789',
                                body='',
                                status=200)
-        self.arth.run(once=True)
+        self.node._autoRefreshThread.run(once=True)
         (httpretty.last_request().method).should.equal('POST')
         (httpretty.last_request().path).should.equal('/api/subscriptionRefresh.xml?id=123456789')
 
