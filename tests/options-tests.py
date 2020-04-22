c1bd8921505f6b097dad75dcc50c127c10f9c79c
Sapan Shah <sapshah@cisco.com>
2020-04-22 00:21:03 -0700

    Add tests for the new options

--- tests/options-tests.py
+++ tests/options-tests.py
@@ -36,3 +36,29 @@ def testPage():
 def testFilter():
     opt = pyaci.options.filter(pyaci.filters.Eq('fvTenant.name', 'cisco'))
     opt['query-target-filter'].should.equal('eq(fvTenant.name,"cisco")')
+
+def testRspSubtreeInclude():
+    opt = pyaci.options.rspSubtreeInclude('relations')
+    opt['rsp-subtree'].should.equal('full')
+    opt['rsp-subtree-include'].should.equal('relations')
+
+def testRspSubtreeClass():
+    opt = pyaci.options.rspSubtreeClass('fvAEPg')
+    opt['rsp-subtree'].should.equal('full')
+    opt['rsp-subtree-class'].should.equal('fvAEPg')
+
+def testRspPropInclude():
+    opt = pyaci.options.rspPropInclude('config-only')
+    opt['rsp-prop-include'].should.equal('config-only')
+
+def testRspSubtreeChildren():
+    opt = pyaci.options.rspSubtreeChildren
+    opt['rsp-subtree'].should.equal('children')
+
+def testRspSubtreeFull():
+    opt = pyaci.options.rspSubtreeFull
+    opt['rsp-subtree'].should.equal('full')
+
+def testSubtreeFilter():
+    opt = pyaci.options.subtreeFilter(pyaci.filters.Eq('fvTenant.name', 'cisco'))
+    opt['rsp-subtree-filter'].should.equal('eq(fvTenant.name,"cisco")')
