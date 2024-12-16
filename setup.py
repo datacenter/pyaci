0e413552da26df57193f8cc8f46bf20099023130
camillo.rossi@gmail.com <camrossi@cisco.com>
2024-12-17 09:27:19 +1100

    python 2.7 compatibility

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='pyaci',
 
-    version='1.0.0-py2',
+    version='1.0.1',
 
     description='Python Bindings for Cisco ACI REST API',
     long_description=long_description,
